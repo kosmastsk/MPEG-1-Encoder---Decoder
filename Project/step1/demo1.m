fc2ab566331b61330372e1be1ad865b95d91c367
kosmastsk <kosmastsk@gmail.com>
2018-01-18 14:43:24 +0200

    THE ENDgit status!

--- Project/step1/demo1.m
+++ Project/step1/demo1.m
@@ -22,126 +22,116 @@ filePattern = fullfile(myFolder, '*.tiff');
 images = dir(filePattern);
 
 %% Preprocessing
-% % Read every image in the folder specified
-% for k = 1 : 1%length(images) % Change to 1 if we want to run for one image
-%     baseFileName = images(k).name;
-%     fullFileName = fullfile(myFolder, baseFileName);
-%     fprintf(1, 'Now reading %s\n', fullFileName);
-%     image = imread(fullFileName);
-% %     figure;
-% %     imshow(image);
-% %     title('Original image');
-% 
-% %     Convert the image to YCrCb
-%     [frameY, frameCr, frameCb] = ccir2ycrcb(image);
-%    
-% %     Inverse function
-%     frameRGB = ycrcb2ccir(frameY, frameCr, frameCb);
-%       The frame that occurs from frameRGB is exactly like the initial image
-% end
+% Read every image in the folder specified
+for k = 1 : length(images) % Change to 1 if we want to run for one image
+    baseFileName = images(k).name;
+    fullFileName = fullfile(myFolder, baseFileName);
+    fprintf(1, 'Now reading %s\n', fullFileName);
+    image = imread(fullFileName);
+    figure;
+    imshow(image);
+    title('Original image');
+
+%     Convert the image to YCrCb
+    [frameY, frameCr, frameCb] = ccir2ycrcb(image);
+   
+%     Inverse function
+    frameRGB = ycrcb2ccir(frameY, frameCr, frameCb);
+      %The frame that occurs from frameRGB is exactly like the initial image
+end
 
 %% Motion Estimator for P frames
-%     
-% % Read the image in which to apply motion estimation "coastguard003.tiff"
-% baseFileName = images(4).name;
-% fullFileName = fullfile(myFolder, baseFileName);
-% fprintf(1, 'Now reading %s\n', fullFileName);
-% image = imread(fullFileName);
-% % figure;
-% % imshow(image);
-% % title('Image in which we apply motion estimation');
-% % Convert the image to YCrCb
-% [frameY, frameCr, frameCb] = ccir2ycrcb(image);
-% 
-% % Read the reference image "coastguard001.tiff"
-% baseFileName = images(2).name;
-% fullFileName = fullfile(myFolder, baseFileName);
-% fprintf(1, 'Now reading %s\n', fullFileName);
-% refImage = imread(fullFileName);
-% % figure;
-% % imshow(image);
-% % title('Reference image for motion estimation');
-% % Convert the image to YCrCb
-% [refFrameY, refFrameCr, refFrameCb] = ccir2ycrcb(refImage);
-% 
-% % frameY now is 360 pixels wide, but we need 352 to apply a 16x16
-% % macroblock size, so we need to delete 4 pixels in each side right and left. The same for the chroma
-% % frames, 2 pixels in each side
-% % frameY(:, 1:4) = 0;
-% % frameY(:, 357:360) = 0;
-% frameY = frameY(:, 5:356);
-% 
-% % frameCr(:, 1:2) = 0;
-% % frameCr(:, 179:180) = 0;
-% frameCr = frameCr(:, 3:178);
-% 
-% % frameCb(:, 1:2) = 0;
-% % frameCb(:, 179:180) = 0;
-% frameCb = frameCb(:, 3:178);
-%   
-% % The number of 16x16 macroblocks that can fit our image
-% max_mBIndex = floor(size(frameY, 1) / 16) * floor(size(frameY, 2) / 16);
-% 
-% for mBIndex = 0 : 0 % max_mBIndex - 1 % minus 1 since we start counting from zero
-%     
-%     % motion estimation P frames
-%     [eMBY, eMBCr, eMBCb, mV] = motEstP(frameY, frameCr, frameCb, mBIndex, refFrameY, refFrameCr, refFrameCb);   
-%     
-%     % Inverse motion estimation P frames
-%     [mBY, mBCr, mBCb] = iMotEstP(eMBY, eMBCr, eMBCb, mBIndex, mV, refFrameY, refFrameCr, refFrameCb);
-% end
+    
+% Read the image in which to apply motion estimation "coastguard003.tiff"
+baseFileName = images(4).name;
+fullFileName = fullfile(myFolder, baseFileName);
+fprintf(1, 'Now reading %s\n', fullFileName);
+image = imread(fullFileName);
+figure;
+imshow(image);
+title('Image in which we apply motion estimation');
+% Convert the image to YCrCb
+[frameY, frameCr, frameCb] = ccir2ycrcb(image);
+
+% Read the reference image "coastguard001.tiff"
+baseFileName = images(2).name;
+fullFileName = fullfile(myFolder, baseFileName);
+fprintf(1, 'Now reading %s\n', fullFileName);
+refImage = imread(fullFileName);
+figure;
+imshow(image);
+title('Reference image for motion estimation');
+% Convert the image to YCrCb
+[refFrameY, refFrameCr, refFrameCb] = ccir2ycrcb(refImage);
+
+% frameY now is 360 pixels wide, but we need 352 to apply a 16x16
+% macroblock size, so we need to delete 4 pixels in each side right and left. The same for the chroma
+% frames, 2 pixels in each side
+
+frameY = frameY(:, 5:356);
+
+frameCr = frameCr(:, 3:178);
+
+frameCb = frameCb(:, 3:178);
+  
+% The number of 16x16 macroblocks that can fit our image
+max_mBIndex = floor(size(frameY, 1) / 16) * floor(size(frameY, 2) / 16);
+
+for mBIndex = 0 : max_mBIndex - 1 % minus 1 since we start counting from zero
+    
+    % motion estimation P frames
+    [eMBY, eMBCr, eMBCb, mV] = motEstP(frameY, frameCr, frameCb, mBIndex, refFrameY, refFrameCr, refFrameCb);   
+    
+    % Inverse motion estimation P frames
+    [mBY, mBCr, mBCb] = iMotEstP(eMBY, eMBCr, eMBCb, mBIndex, mV, refFrameY, refFrameCr, refFrameCb);
+end
 
 %% Motion Estimator for B frames
     
-% % Read the image in which to apply motion estimation "coastguard003.tiff"
-% baseFileName = images(4).name;
-% fullFileName = fullfile(myFolder, baseFileName);
-% fprintf(1, 'Now reading %s\n', fullFileName);
-% image = imread(fullFileName);
-% % Convert the image to YCrCb
-% [frameY, frameCr, frameCb] = ccir2ycrcb(image);
-% 
-% % Read the forward frame"coastguard001.tiff" % picture in the past
-% baseFileName = images(2).name;
-% fullFileName = fullfile(myFolder, baseFileName);
-% fprintf(1, 'Now reading %s\n', fullFileName);
-% forwFrame = imread(fullFileName);
-% % Convert the image to YCrCb
-% [forwFrameY, forwFrameCr, forwFrameCb] = ccir2ycrcb(forwFrame);
-% 
-% % Read the backward frame "coastguard004.tiff" % picture in the future
-% baseFileName = images(5).name;
-% fullFileName = fullfile(myFolder, baseFileName);
-% fprintf(1, 'Now reading %s\n', fullFileName);
-% backwFrame = imread(fullFileName);
-% % Convert the image to YCrCb
-% [backwFrameY, backwFrameCr, backwFrameCb] = ccir2ycrcb(backwFrame);
-% 
-% % frameY now is 360 pixels wide, but we need 352 to apply a 16x16
-% % macroblock size, so we need to delete 4 pixels in each side right and left. The same for the chroma
-% % frames, 2 pixels in each side
-% % frameY(:, 1:4) = 0;
-% % frameY(:, 357:360) = 0;
-% frameY = frameY(:, 5:356);
-% 
-% % frameCr(:, 1:2) = 0;
-% % frameCr(:, 179:180) = 0;
-% frameCr = frameCr(:, 3:178);
-% 
-% % frameCb(:, 1:2) = 0;
-% % frameCb(:, 179:180) = 0;
-% frameCb = frameCb(:, 3:178);
-%   
-% % The number of 16x16 macroblocks that can fit our image
-% max_mBIndex = floor(size(frameY, 1) / 16) * floor(size(frameY, 2) / 16);
-% 
-% for mBIndex = 10 :10% max_mBIndex - 1 % minus 1 since we start counting from zero
-%     % motion estimation B frames
-%     [eMBY, eMBCr, eMBCb, mV, MBY] = motEstB(frameY, frameCr, frameCb, mBIndex, backwFrameY, backwFrameCr, backwFrameCb, forwFrameY, forwFrameCr, forwFrameCb);
-% 
-%     % Inverse motion estimation B frames
-%     [mBY, mBCr, mBCb] = iMotEstB(eMBY, eMBCr, eMBCb, mBIndex, mV, backwFrameY, backwFrameCr, backwFrameCb, forwFrameY, forwFrameCr, forwFrameCb);
-% end
+% Read the image in which to apply motion estimation "coastguard003.tiff"
+baseFileName = images(4).name;
+fullFileName = fullfile(myFolder, baseFileName);
+fprintf(1, 'Now reading %s\n', fullFileName);
+image = imread(fullFileName);
+% Convert the image to YCrCb
+[frameY, frameCr, frameCb] = ccir2ycrcb(image);
+
+% Read the forward frame"coastguard001.tiff" % picture in the past
+baseFileName = images(2).name;
+fullFileName = fullfile(myFolder, baseFileName);
+fprintf(1, 'Now reading %s\n', fullFileName);
+forwFrame = imread(fullFileName);
+% Convert the image to YCrCb
+[forwFrameY, forwFrameCr, forwFrameCb] = ccir2ycrcb(forwFrame);
+
+% Read the backward frame "coastguard004.tiff" % picture in the future
+baseFileName = images(5).name;
+fullFileName = fullfile(myFolder, baseFileName);
+fprintf(1, 'Now reading %s\n', fullFileName);
+backwFrame = imread(fullFileName);
+% Convert the image to YCrCb
+[backwFrameY, backwFrameCr, backwFrameCb] = ccir2ycrcb(backwFrame);
+
+% frameY now is 360 pixels wide, but we need 352 to apply a 16x16
+% macroblock size, so we need to delete 4 pixels in each side right and left. The same for the chroma
+% frames, 2 pixels in each side
+
+frameY = frameY(:, 5:356);
+
+frameCr = frameCr(:, 3:178);
+
+frameCb = frameCb(:, 3:178);
+  
+% The number of 16x16 macroblocks that can fit our image
+max_mBIndex = floor(size(frameY, 1) / 16) * floor(size(frameY, 2) / 16);
+
+for mBIndex = 1 : max_mBIndex - 1 % minus 1 since we start counting from zero
+    % motion estimation B frames
+    [eMBY, eMBCr, eMBCb, mV, MBY] = motEstB(frameY, frameCr, frameCb, mBIndex, backwFrameY, backwFrameCr, backwFrameCb, forwFrameY, forwFrameCr, forwFrameCb);
+
+    % Inverse motion estimation B frames
+    [mBY, mBCr, mBCb] = iMotEstB(eMBY, eMBCr, eMBCb, mBIndex, mV, backwFrameY, backwFrameCr, backwFrameCb, forwFrameY, forwFrameCr, forwFrameCb);
+end
 
 %% All the next function are applied to a specific macroblock of an image to prove their functionality
 % We are using every function the one after the other, and in the end the
@@ -214,7 +204,7 @@ for mBIndex = 0 : 0 % max_mBIndex - 1 % minus 1 since we start counting from zer
     quantBlockCr = quantizePB(eMBCrDCT, qTable, qScale);
 
     % RLE
-    runSymbolsY = runLength(quantBlockY);
+    runSymbolsY = runLength(quantBlockY);     
     runSymbolsCb = runLength(quantBlockCb);
     runSymbolsCr = runLength(quantBlockCr);
     
@@ -226,7 +216,7 @@ for mBIndex = 0 : 0 % max_mBIndex - 1 % minus 1 since we start counting from zer
 %% Inverse procedure
     
     % iVLC
-    irunSymbolsY = ivlc(vlcStreamY);
+    irunSymbolsY = ivlc(vlcStreamY)
     irunSymbolsCb = ivlc(vlcStreamCb);
     irunSymbolsCr = ivlc(vlcStreamCr);
     
@@ -263,6 +253,7 @@ for mBIndex = 0 : 0 % max_mBIndex - 1 % minus 1 since we start counting from zer
     [mBY, mBCr, mBCb] = iMotEstP(ieMBY, ieMBCr, ieMBCb, mBIndex, mV, refFrameY, refFrameCr, refFrameCb);
     figure;
     imshow(mBY);
+    title('Reconstructed macroblock 0 - Y channel - “coastguard003.tiff”');
 end
 
 %%  END 
